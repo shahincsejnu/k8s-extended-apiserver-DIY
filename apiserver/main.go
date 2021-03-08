7d87e706b5627518fbe9489504a6cd62ee00abb0
sahadat_hossain <sahadat_hossain@appscode.com>
2021-03-08 20:02:15 +0600

    added step-3-extended-apiserver

--- apiserver/main.go
+++ apiserver/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/shahincsejnu/k8s-extended-apiserver-DIY/lib/certstore"
 	"github.com/shahincsejnu/k8s-extended-apiserver-DIY/lib/server"
 	"github.com/spf13/afero"
+	"io"
 	"k8s.io/client-go/util/cert"
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -17,12 +22,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var proxy = false
+	flag.BoolVar(&proxy, "proxy-request", proxy, "forward requests to database extended apiserver")
+	flag.Parse()
+	fmt.Println(proxy)
+
 	fs := afero.NewOsFs()
 	store, err := certstore.NewCertStore(fs, "/tmp/k8s-extended-apiserver-DIY")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = store.NewCA("apiserver")
+	err = store.InitCA("apiserver")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -49,6 +59,43 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	rhStore, err := certstore.NewCertStore(fs, "/tmp/k8s-extended-apiserver-DIY")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = rhStore.InitCA("requestheader")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rhClientCert, rhClientKey, err := rhStore.NewClientCertPair(cert.AltNames{
+		DNSNames: []string{"apiserver"}, // because apiserver is making the calls to database eas
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = rhStore.Write("apiserver", rhClientCert, rhClientKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	rhCert, err := tls.LoadX509KeyPair(rhStore.CertFile("apiserver"), rhStore.KeyFile("apiserver"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	easCACertPool := x509.NewCertPool()
+	if proxy {
+		easStore, err := certstore.NewCertStore(fs, "/tmp/k8s-extended-apiserver-DIY")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = easStore.LoadCA("database")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		easCACertPool.AppendCertsFromPEM(easStore.CACertBytes())
+	}
+
 	cfg := server.Config{
 		Address: "127.0.0.1:8443",
 		CACertFiles: []string{
@@ -65,6 +112,45 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "Resource: %v\n", vars["resource"])
 	})
+
+	if proxy {
+		fmt.Println("Entered in forwarding")
+		r.HandleFunc("/database/{resource}", func(w http.ResponseWriter, r *http.Request) {
+			tr := &http.Transport{
+				MaxIdleConnsPerHost: 10,
+				TLSClientConfig: &tls.Config{
+					Certificates: []tls.Certificate{rhCert},
+					RootCAs:      easCACertPool,
+				},
+			}
+			client := http.Client{
+				Transport: tr,
+				Timeout:   time.Duration(30 * time.Second),
+			}
+
+			u := *r.URL
+			u.Scheme = "https"
+			u.Host = "127.0.0.2:8443"
+			fmt.Printf("forwarding request to %v\n", u.String())
+
+			req, _ := http.NewRequest(r.Method, u.String(), nil)
+			if len(r.TLS.PeerCertificates) > 0 {
+				req.Header.Set("X-Remote-User", r.TLS.PeerCertificates[0].Subject.CommonName)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "error: %v\n", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			w.WriteHeader(http.StatusOK)
+			io.Copy(w, resp.Body)
+		})
+	}
+
 	r.HandleFunc("/", handler)
 	srv.ListenAndServe(r)
 }
